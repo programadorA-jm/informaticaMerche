ec532694f14f73188caa1ac3a37f6190f52e6c81
merche <informatica.merche.merino@gmail.com>
2017-04-27 18:03:53 +0200

    ANTES DE SUBIRLO AL REMOTO POR PRIMERA VEZ

---
+++ src/proyectogitprogramadora/ProyectoGitProgramadorA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogitprogramadora;
+
+/**
+ *
+ * @author programadorA
+ */
+public class ProyectoGitProgramadorA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // este método lo ha desarrollado PROGRAMADORA
+    }
+    
+}
